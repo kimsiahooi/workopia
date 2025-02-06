324f8da2a73238e025dd352e9d6f32f662a18140
Ooi Kim Siah <kimsiahooi@gmail.com>
2025-02-06 20:55:18 +0800

    54

--- resources/js/Pages/Jobs/Index.vue
+++ resources/js/Pages/Jobs/Index.vue
@@ -1,8 +1,9 @@
 <template>
     <Head title="Job Listings" />
     <h1>Available Jobs</h1>
-    <ul v-if="jobs.length">
-        <li v-for="job in jobs" :key="job.title">{{ job.title }}</li>
+    <ul>
+        <li v-if="jobs.length" v-for="job in jobs" :key="job.title">{{ job.title }}</li>
+        <li v-else>No jobs available</li>
     </ul>
 </template>
 
