527f78dbbd7b7bcdcc81a8f0887540cb51242fd8
Ooi Kim Siah <kimsiah@fomatictech.com>
2025-02-06 16:49:10 +0800

    41

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -17,12 +17,13 @@
                 </a>
                 <ButtonLink :url="route('jobs.create')" icon="edit">Create Job</ButtonLink>
             </nav>
-            <button id="hamburger" class="text-white md:hidden flex items-center">
+            <button id="hamburger" class="text-white md:hidden flex items-center" @click="toggleMenu">
                 <i class="fa fa-bars text-2xl"></i>
             </button>
         </div>
         <!-- Mobile Menu -->
-        <nav id="mobile-menu" class="hidden md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2">
+        <nav id="mobile-menu"
+            :class="`${!menuInOpen ? 'hidden' : ''} md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2`">
             <NavLink :url="route('jobs.index')" :active="checkActiveRoute('jobs.index')" mobile>
                 All Jobs
             </NavLink>
@@ -38,7 +39,7 @@
 <script setup lang="ts">
 import { Link, usePage } from '@inertiajs/vue3';
 import { route } from 'ziggy-js';
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import NavLink from './NavLink.vue';
 import ButtonLink from './ButtonLink.vue';
 
@@ -47,6 +48,12 @@ const page = usePage<{
         current_route: string
     }
 }>()
+
+const menuInOpen = ref(false);
+
+const toggleMenu = () => {
+    menuInOpen.value = !menuInOpen.value
+}
 
 const computedCurrentRoute = computed(() => page.props.ziggy?.current_route);
 
